1c31bc2a2e3693dbb33468ded4542eec776a7ec0
Vinicios <vinicios.ldcampos@gmail.com>
2025-07-12 17:25:43 -0300

    Exercicio 2 - Segunda forma da função main

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ const player2 = {
     MANOBRABILIDADE : 4,
     PODER : 4,
     PONTOS: 0,
-}
+};
 
+// Async significa que a função não bloqueará a execução do restante do código enquanto aguarda o resultado de uma operação assíncrona.
 // A função main abaixo é chamada de função de entrada e vai chamar as demais funções.
 // Forma de chamar a função main.
-async function main_primeiro_Exercicio() {
-    console.log("1° Exercício: Hello");
-}
-main_primeiro_Exercicio();
+// A colocação de '(' antes do async  o fechamento do parenteses junto com abre e fecha parenteses ')()' depois da função mostra que aquela função é auto-invocavel.
+(async function main_segundo_Exercicio() {
+    console.log("2° Exercício: Hello");
+})();
