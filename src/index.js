f25d66b20d9bc57411e13a2774481720bb2ae8cb
Vinicios <vinicios.ldcampos@gmail.com>
2025-07-12 17:07:10 -0300

    Exercicio 1 - Criando a função main

--- src/index.js
+++ src/index.js
@@ -14,3 +14,10 @@ const player2 = {
     PODER : 4,
     PONTOS: 0,
 }
+
+// A função main abaixo é chamada de função de entrada e vai chamar as demais funções.
+// Forma de chamar a função main.
+async function main_primeiro_Exercicio() {
+    console.log("1° Exercício: Hello");
+}
+main_primeiro_Exercicio();
